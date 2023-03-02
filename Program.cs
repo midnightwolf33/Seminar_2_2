bec9edfc776781f4a0f516f4b30a34ab944c62df
midnight33 <gra33@bk.ru>
2023-03-02 18:37:04 +0300

    Задача 2 семинара 2 - готова

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.Write("Введите число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int Length2 = num2.ToString().Length;
+if (Length2 >= 3){
+    while (num2 > 999)
+    {
+    num2 = num2 / 10;
+    }
+    int result = num2 % 10;
+    Console.WriteLine("Третья цифра: " + result);
+}   
+else {
+    Console.WriteLine("Третьей цифры нет");
+}
